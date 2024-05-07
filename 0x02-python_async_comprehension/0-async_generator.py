3d5f254092eb4f4c4674dd7289892cfecf813295
Keglo Dodzi <stephanekeglo@gmail.com>
2024-05-07 23:25:56 +0000

    Fix coroutine type annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """async_generator module"""
 import asyncio
 import random
-from typing import Iterator
+from typing import Generator
 
 
-async def async_generator() -> Iterator[float]:
+async def async_generator() -> Generator[float, None, None]:
     """loop 10 times and yield a random number between 0 and 10"""
     for i in range(10):
         await asyncio.sleep(1)
