94c8ddfe9bbf7b9c8e4e555a1918c31b76330525
Keglo Dodzi <stephanekeglo@gmail.com>
2024-05-27 20:27:48 +0000

    Add test_client.py Task #6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,9 +3,9 @@
 """
 import unittest
 from client import GithubOrgClient
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, PropertyMock
 from parameterized import parameterized, param
-from typing import Dict, NoReturn
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -15,13 +15,19 @@ class TestGithubOrgClient(unittest.TestCase):
         param(org="abc", result={})
     ])
     @patch('client.get_json')
-    def test_org(self,
-                 get_json_mock: MagicMock,
-                 org: str,
-                 result: Dict) -> NoReturn:
+    def test_org(self, get_json_mock, org, result):
         """test GitHubOrgClient org method"""
         get_json_mock.return_value = result
         url = f"https://api.github.com/orgs/{org}"
         client = GithubOrgClient(org)
         self.assertEqual(client.org, result)
         get_json_mock.assert_called_once_with(url)
+
+    def test_public_repos_url(self):
+        """test GithubOrgClient._public_repos_url"""
+        with patch.object(GithubOrgClient, 'org',
+                          new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = TEST_PAYLOAD[0][0]
+            client = GithubOrgClient("google")
+            expected = "https://api.github.com/orgs/google/repos"
+            self.assertEqual(client._public_repos_url, expected)
