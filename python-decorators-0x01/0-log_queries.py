0dca2a57173d14c562fdf9546b0a79c143ff11a6
Dodzi Keglo <stephanekeglo@gmail.com>
2025-07-12 14:21:12 +0000

    Log database queries

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""A decorator that logs database queries executed by any function.
+"""
+import sqlite3
+import functools
+
+#### decorator to lof SQL queries
+
+
+def log_queries(func):
+    """Log database queries"""
+
+    functools.wraps(func)
+    def wrapper(**kargs):
+        print(f"Running: {kargs}")
+        return func(**kargs)
+
+    return wrapper
+
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
