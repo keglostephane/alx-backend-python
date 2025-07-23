77121291549795200abd23c14efa778a44cb21ff
Dodzi Keglo <stephanekeglo@gmail.com>
2025-07-23 15:05:47 +0000

    Add conversation attribute to Message Model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -25,10 +25,13 @@ class User(AbstractUser):
         "Phone",
         max_length=15,
         blank=True,
-        validators=[RegexValidator(
-            regex=r"^\+(?:[0-9]){6,14}[0-9]$",
-            message="phone number must start with '+' and contains no spaces. \
-            up to 15 digits allowed.")])
+        validators=[
+            RegexValidator(
+                regex=r"^\+(?:[0-9]){6,14}[0-9]$",
+                message=
+                "phone number must start with '+' and contains no spaces. \
+            up to 15 digits allowed.")
+        ])
     role = models.CharField(max_length=5,
                             choices=Role.choices,
                             default=Role.GUEST)
@@ -39,28 +42,6 @@ class User(AbstractUser):
 
     class Meta:
         indexes = [models.Index(fields=["email"], name="email_idx")]
-
-
-class Message(models.Model):
-    message_id = models.UUIDField(primary_key=True,
-                                  default=uuid.uuid4,
-                                  editable=False)
-    sender = models.ForeignKey(settings.AUTH_USER_MODEL,
-                               on_delete=models.CASCADE,
-                               related_name="sent_messages")
-    recipient = models.ForeignKey(settings.AUTH_USER_MODEL,
-                                  on_delete=models.CASCADE,
-                                  related_name="received_messages")
-    message_body = models.TextField("message")
-    sent_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"From {self.sender.get_full_name()} at \
-        {datetime.isoformat(self.sent_at)}"
-
-    def clean(self):
-        if self.sender == self.recipient:
-            raise ValidationError("Sender must be different from Recipient")
 
 
 class Conversation(models.Model):
@@ -91,3 +72,28 @@ class ConversationParticipant(models.Model):
 
     class Meta:
         unique_together = ["conversation_id", "user_id"]
+
+
+class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True,
+                                  default=uuid.uuid4,
+                                  editable=False)
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE,
+                               related_name="sent_messages")
+    recipient = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                  on_delete=models.CASCADE,
+                                  related_name="received_messages")
+    conversation = models.ForeignKey(Conversation,
+                                     on_delete=models.CASCADE,
+                                     related_name="messages")
+    message_body = models.TextField("message")
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"From {self.sender.get_full_name()} at \
+        {datetime.isoformat(self.sent_at)}"
+
+    def clean(self):
+        if self.sender == self.recipient:
+            raise ValidationError("Sender must be different from Recipient")
