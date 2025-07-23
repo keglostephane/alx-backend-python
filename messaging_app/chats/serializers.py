66c94c9ce16dcd3aad5cff9e114d16feb094cd6d
Dodzi Keglo <stephanekeglo@gmail.com>
2025-07-23 15:08:04 +0000

    Rewrite serializers to use ModelSerializer class

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,69 +1,76 @@
+from chats.models import Conversation, Message, User
 from rest_framework import serializers
-from .models import User, Message, Conversation
 
 
-class UserSerializer(serializers.Serializer):
+class UserSerializer(serializers.ModelSerializer):
 
-    user_id = serializers.UUIDField(read_only=True)
-    first_name = serializers.CharField("First Name", max_length=150)
-    last_name = serializers.CharField("Last Name", max_length=150)
-    email = serializers.EmailField()
-    full_name = serializers.SerializerMethodField()
-    password_hash = serializers.CharField("Password",
-                                          max_length=255,
-                                          write_only=True)
-    phone_number = serializers.CharField("Phone",
-                                         max_length=20,
-                                         allow_blank=True)
-    role = serializers.ChoiceField(choices=User.Role.choices,
-                                   default=User.Role.GUEST)
-    created_at = serializers.DateTimeField(read_only=True)
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email", "phone_number", "role"]
+        read_only_fields = ["user_id", "created_at"]
 
-    def get_full_name(self, obj):
-        return f"{obj.get_full_name()}"
-    
-    def validate_phone_number(self, value):
-        if not value.isdigit():
-            raise serializers.ValidationError("Incorrect phone number")
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = serializers.SlugRelatedField(many=False, slug_field="email")
+    recipient = serializers.SlugRelatedField(many=False, slug_field="email")
+    conversation = serializers.PrimaryKeyRelatedField(
+        queryset=Conversation.objects.all())
+
+    class Meta:
+        model = Message
+        fields = ["sender", "recipient", "message_body", "conversation"]
+        read_only_fields = ["message_id", "sent_at"]
 
     def create(self, validated_data):
-        return User.objects.create(**validated_data)
+        conversation = validated_data.get("conversation")
+        sender = validated_data.get("sender")
+        recipient = validated_data.get("recipient")
 
-    def update(self, instance, validated_data):
-        instance.first_name = validated_data.get("first_name",
-                                                 instance.first_name)
-        instance.last_name = validated_data.get("last_name",
-                                                instance.last_name)
-        instance.email = validated_data.get("email", instance.email)
-        instance.phone_number = validated_data.get("phone_number",
-                                                   instance.phone_number)
-        instance.role = validated_data.get("role", instance.role)
-        instance.save()
-        return instance
+        if not conversation.participants.filter(user_id=sender).exists():
+            raise serializers.ValidationError(
+                "Sender must be part of the conversation.")
 
+        if not conversation.participants.filter(user_id=recipient).exists():
+            raise serializers.ValidationError(
+                "Recipient must be part of the conversation.")
 
-class MessageSerializer(serializers.Serializer):
-    message_id = serializers.UUIDField(read_only=True)
-    sender_id = UserSerializer(read_only=True)
-    message_body = serializers.CharField()
-    sent_at = serializers.DateTimeField(read_only=True)
-
-    def create(self, validated_data):
         return Message.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
+        conversation = validated_data.get("conversation",
+                                          instance.conversation)
+        sender = validated_data.get("sender", instance.sender)
+        recipient = validated_data.get("recipient", instance.recipient)
+
+        if not conversation.participants.filter(user_id=sender).exists():
+            raise serializers.ValidationError(
+                "Sender must be part of the conversation.")
+
+        if not conversation.participants.filter(user_id=recipient).exists():
+            raise serializers.ValidationError(
+                "Recipient must be part of the conversation.")
+
+        instance.sender = sender
+        instance.recipient = recipient
+        instance.conversation = conversation
         instance.message_body = validated_data.get("message_body",
                                                    instance.message_body)
         instance.save()
+
         return instance
 
 
-class ConversationSerializer(serializers.Serializer):
-    conversation_id = serializers.UUIDField(read_only=True)
-    participants_id = UserSerializer(many=True, read_only=True)
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = serializers.SlugRelatedField(many=True,
+                                                slug_field="email",
+                                                queryset=User.objects.all())
+    messages = serializers.SerializerMethodField()
 
-    def create(self, validated_data):
-        return Conversation.objects.create(**validated_data)
+    class Meta:
+        model = Conversation
+        fields = ["participants"]
+        read_only_fields = ["conversation_id", "created_at"]
 
-    def update(self, instance, validated_data):
-        pass
+    def get_messages(self, obj):
+        messages = obj.messages.all()
+        return MessageSerializer(messages, many=True).data
