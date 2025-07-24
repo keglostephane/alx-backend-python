2abf09e027dba9523855298c7cad1c21a043c7df
Dodzi Keglo <stephanekeglo@gmail.com>
2025-07-24 23:17:29 +0000

    Rewrite viewsets using ModelViewSet

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -5,64 +5,61 @@ from rest_framework import filters, status, viewsets
 from rest_framework.response import Response
 
 
-class MessageViewSet(viewsets.ViewSet):
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ["subject", "message_body"]
 
-    def list(self, request, conversation_id=None):
-        if conversation_id:
-            queryset = Message.objects.filter(
-                conversation__conversation_id=conversation_id)
-        else:
-            queryset = Message.objects.all()
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        conversation_id = self.kwargs.get("conversation_id")
+        search_query = self.request.query_params.get("search", None)
 
-        search_query = request.query_params.get("search", None)
+        if conversation_id:
+            queryset = queryset.filter(
+                conversation__conversation_id=conversation_id)
 
         if search_query:
             queryset = queryset.filter(
                 Q(subject__icontains=search_query)
                 | Q(message_body__icontains=search_query))
 
-        serializer = MessageSerializer(queryset, many=True)
-        return Response(serializer.data, status.HTTP_200_OK)
+        return queryset
 
-    def create(self, request, conversation_id):
+    def create(self, request, conversation_id, **kwargs):
         try:
             conversation = Conversation.objects.get(
                 conversation_id=conversation_id)
         except Conversation.DoesNotExist:
             return Response({"message": "Conversation not found."},
-                            status.HTTP_404_NOT_FOUND)
+                            status=status.HTTP_404_NOT_FOUND)
 
-        serializer = MessageSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save(conversation=conversation)
-            return Response(serializer.data, status.HTTP_201_CREATED)
-        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(conversation=conversation)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-class ConversationViewSet(viewsets.ViewSet):
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ["participants__first_name", "participants__last_name"]
 
-    def list(self, request):
-        queryset = Conversation.objects.all()
-        search_query = request.query_params.get("search", None)
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.query_params.get("search", None)
 
         if search_query:
             queryset = queryset.filter(
                 Q(participants__first_name__icontains=search_query)
                 | Q(participants__last_name__icontains=search_query))
 
-        serializer = ConversationSerializer(queryset, many=True)
-        return Response(serializer.data, status.HTTP_200_OK)
+        return queryset
 
-    def create(self, request):
-        serializer = ConversationSerializer(data=request.data)
-
-        if not serializer.is_valid():
-            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
-
+    def create(self, request, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data, status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
